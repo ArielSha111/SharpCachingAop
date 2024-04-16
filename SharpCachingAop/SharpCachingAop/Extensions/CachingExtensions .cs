5bf56ec802a95c59bacc6745b776f540cc9c045f
ArielSha111 <ArielSharkie1@gmail.com>
2024-04-16 21:39:47 +0300

    preps

---
+++ SharpCachingAop/SharpCachingAop/Extensions/CachingExtensions .cs
@@ -0,0 +1,18 @@
+﻿using CachingAop.Caching;
+using CachingAop.Configuration;
+
+namespace SharpCachingAop.Extensions;
+
+public static class CachingProviderExtensions
+{
+    public static async Task<T> GetOrSetAsync<T>(this ICachingProvider cachingProvider, CachingConfigurationBlock _configurationBlock, string fullCacheKey)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static T GetOrSet<T>(this ICachingProvider cachingProvider, CachingConfigurationBlock _configurationBlock, string fullCacheKey)
+    {
+        throw new NotImplementedException();
+    }
+}
+
